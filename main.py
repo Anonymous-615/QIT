86d82fc3c44a9d491a2941ea81029b118554073c
Anonymous-615 <157101567+Anonymous-615@users.noreply.github.com>
2024-05-27 15:45:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     print("dataset:", datanames[0])
     datas = load_data(datanames[0], device)
     
-    # optional:Redundancy-Elimination Performance
+    ## optional:Redundancy-Elimination Performance
     # origin_edge=datas[0].edge_index
     # redun_eli_edge=datas[3]
     # boost=(1-len(redun_eli_edge)/len(origin_edge))*100
