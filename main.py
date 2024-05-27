a458b2260df964384a9747c3b0d889fba86be6e1
Anonymous-615 <157101567+Anonymous-615@users.noreply.github.com>
2024-05-27 15:42:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ def main():
     device = torch.device('cuda')
     print("dataset:", datanames[0])
     datas = load_data(datanames[0], device)
-
+    
+    # optional:Redundancy-Elimination Performance
+    # origin_edge=datas[0].edge_index
+    # redun_eli_edge=datas[3]
+    # boost=(1-len(redun_eli_edge)/len(origin_edge))*100
+    # print('Redundancy-Elimination Boost: %.2f'%boost)
+    
     model = load_model(modelnames[0], datas, device)
     start = time.time()
     train(model, datas, device)
